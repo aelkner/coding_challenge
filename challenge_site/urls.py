b834e1d9aa958b213fe92497d577a002b7e265d8
Alan Elkner <aelkner@gmail.com>
2015-10-03 16:56:44 -0400

    configured site to use challenge_app
    - added challenge_app to installed apps
    - added HomeView to challenge_app.views (simply hello world so far)
    - added templates/challenge_app/home.html to challenge_app
    - pointed '/' to challenge_app.views.HomeView
    - added basic test to challenge_app.tests
    
    commands run (assuming current directory is project dir and is sourced):
    
    python manage.py runserver (http://localhost:8000/ returns hello world)
    python manage.py test challenge_app (basic test passes)

--- challenge_site/urls.py
+++ challenge_site/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from challenge_app.views import HomeView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
