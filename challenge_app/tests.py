a90e7311cf1f0106776810bcc341e52e4ea59397
Alan Elkner <aelkner@gmail.com>
2015-10-04 12:41:25 -0400

    changed HomeView to use HomeForm and convert_integer_to_roman_numerals
    - changed HomeView to be a form view with new form, HomeForm, to receive and
      validate input_number and display convert_integer_to_roman_numerals result
    - new version of the template uses javascript to make sure input_number is
      always in focus and completely selected to make it convenient to test many
      numbers in a row
    - automated tests to check form errors and a couple correct conversions
    - added BeautifulSoup to the requirements for client testing
    - the html template uses javascript rather than jquery simply to avoid adding
      an unnecessary dependency for such a small project
    
    commands run (assuming current directory is project dir and is sourced):
    
    pip install -r requirements.txt (to get BeautifulSoup installed)

--- challenge_app/tests.py
+++ challenge_app/tests.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 from django.test import TestCase
 
 import views
@@ -26,3 +28,67 @@ class UnitTest(TestCase):
         assert_convert_integer_to_roman_numerals(867,   'DCCCLXVII')
         assert_convert_integer_to_roman_numerals(1998,  'MCMXCVIII')
         assert_convert_integer_to_roman_numerals(3999,  'MMMCMXCIX')
+
+
+class ClientTest(TestCase):
+    def test_convert_integer_to_roman_numerals(self):
+        INVALID_ERROR = 'Please enter an integer between 1 and 3999.'
+
+        # Doing a get on the view yields a form with a blank input_number
+        # field and no results.
+        self.response = self.client.get('/')
+        soup = BeautifulSoup(self.response.content)
+        self.assertIsNone(soup.find(id='id_input_number').get('value'))
+        self.assertIsNone(soup.find(id='id_roman_numerals'))
+
+        # Posting no input_number yields the missing field error.
+        self.response = self.client.post('/', {})
+        soup = BeautifulSoup(self.response.content)
+        self.assertEqual(
+            soup.select('.errorlist li')[0].string,
+            'This field is required.'
+        )
+        self.assertIsNone(soup.find(id='id_roman_numerals'))
+
+        # Posting a non-integer yields the invalid field error.
+        self.response = self.client.post('/', {'input_number': 'foo'})
+        soup = BeautifulSoup(self.response.content)
+        self.assertEqual(
+            soup.select('.errorlist li')[0].string, INVALID_ERROR
+        )
+        self.assertIsNone(soup.find(id='id_roman_numerals'))
+
+        # Posting an integer < 1 yields the invalid field error.
+        self.response = self.client.post('/', {'input_number': '0'})
+        soup = BeautifulSoup(self.response.content)
+        self.assertEqual(
+            soup.select('.errorlist li')[0].string, INVALID_ERROR
+        )
+        self.assertIsNone(soup.find(id='id_roman_numerals'))
+
+        # Posting an integer > 3999 yields the invalid field error.
+        self.response = self.client.post('/', {'input_number': '4000'})
+        soup = BeautifulSoup(self.response.content)
+        self.assertEqual(
+            soup.select('.errorlist li')[0].string, INVALID_ERROR
+        )
+        self.assertIsNone(soup.find(id='id_roman_numerals'))
+
+        # Posting a valid number yields no errors but rather a
+        # successfully converted result.
+        self.response = self.client.post('/', {'input_number': '1'})
+        soup = BeautifulSoup(self.response.content)
+        self.assertFalse(soup.select('.errorlist li'))
+        self.assertEqual(
+            soup.find(id='id_roman_numerals').string.strip(),
+            'I'
+        )
+
+        # Posting another valid number yields success again.
+        self.response = self.client.post('/', {'input_number': '3999'})
+        soup = BeautifulSoup(self.response.content)
+        self.assertFalse(soup.select('.errorlist li'))
+        self.assertEqual(
+            soup.find(id='id_roman_numerals').string.strip(),
+            'MMMCMXCIX'
+        )
--- challenge_app/views.py
+++ challenge_app/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.views import generic
 
 
@@ -50,5 +51,62 @@ def convert_integer_to_roman_numerals(input_number):
     return ''
 
 
-class HomeView(generic.TemplateView):
+class HomeForm(forms.Form):
+    input_number = forms.CharField(
+        label='Integer number',
+        widget = forms.TextInput(attrs={
+            'placeholder': "Please enter an integer between 1 and 3999",
+            'size': 50,
+        })
+    )
+
+    def clean_input_number(self):
+        """
+        Make sure the input number is a number between 1 and 3999.
+        """
+        input_number = self.cleaned_data.get("input_number", "")
+        if (
+            input_number.isdigit()
+            and int(input_number) > 0
+            and int(input_number) < 4000
+        ):
+            return input_number
+        raise forms.ValidationError(
+            "Please enter an integer between 1 and 3999."
+        )
+
+
+class HomeView(generic.FormView):
+    """
+    This view presents a form to the user with an input box for entering an
+    integer between 1 and 3999 and a button to convert the number into roman
+    numerals and display that result.
+    """
     template_name = 'challenge_app/home.html'
+    form_class = HomeForm
+
+    def form_valid(self, form):
+        """
+        In order for the user to be able to continue to use the converter
+        after successfully filling in the form with valid input, call
+        form_invalid to avoid the redirect to success_url.
+        """
+        return self.form_invalid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        """
+        If the form is passed, get the input_number from its cleaned_data and
+        convert it to roman numerals, putting the result into context_data
+        under the key, 'roman', for the template to use in displaying the
+        answer.
+        """
+        context_data = super(HomeView, self).get_context_data(*args, **kwargs)
+        cleaned_data = getattr(kwargs['form'], 'cleaned_data', {})
+        input_number = cleaned_data.get('input_number', '')
+        if input_number:
+            context_data['roman'] = convert_integer_to_roman_numerals(
+                int(input_number)
+            )
+        else:
+            context_data['roman'] = ''
+        return context_data
